5f1b5d0a4ccc743068fe680f0a6364e14b4d084e
gabrielemarino98 <58908810+gabrielemarino98@users.noreply.github.com>
2024-09-20 16:06:08 +0200

    first commit

---
+++ scacchi.py
@@ -0,0 +1,31 @@
+chessboard = [[0 for _ in range(8)] for _ in range(8)]
+
+gpios = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for gpio in gpios:
+    # To read the state
+      state = GPIO.input(gpio)
+
+import RPi.GPIO as GPIO
+import chess
+
+def make_move(board, move_uci):
+    """
+    This function takes a chess board and a move in UCI format as arguments,
+    then makes the move on the board.
+    
+    :param board: a chess.Board object
+    :param move_uci: a move in UCI format (e.g., 'e2e4')
+    """
+    move = chess.Move.from_uci(move_uci)  # Convert the UCI string to a move
+    if move in board.legal_moves:  # Check if the move is legal
+        board.push(move)  # Make the move
+        print(board)  # Print the updated board
+    else:
+        print(f"Illegal move: {move_uci}")
+
+# Example usage
+board = chess.Board()
+make_move(board, "e2e5")  #illegal move
+make_move(board, "e2e4")  # e2 to e4
+make_move(board, "e7e5")  # e7 to e5
---
+++ startchess.py
@@ -0,0 +1,3 @@
+#!/bin/bash
+source myenv/bin/activate  # Activate the virtual environment
+python3 scacchi.py     # Run your Python script
